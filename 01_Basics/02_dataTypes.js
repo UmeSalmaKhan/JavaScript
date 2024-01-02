aadc9a245779d650192dafd5abc374bb485a1652
UmeSalmaKhan <68366104+UmeSalmaKhan@users.noreply.github.com>
2024-01-02 08:17:57 +0000

    Variable

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3+3); // Can run with ";"
+console.log("Salma") // Can run without ";"
+
+let name = "Salma"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => huge nubmers->(123445789876543...)
+// string => ""
+// boolean => true/false
+// null => standalone value (null)->empty
+// undefined => not defined variable
+// symbol => unique (Its more in React implement, also figma tools. It used for uniqueness)
+
+//object
+
+console.log(typeof);// it used to check the type of datatype
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
