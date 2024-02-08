b8396c84901abbbc9aa27bd956c6352cfc6b91a1
UmeSalmaKhan <68366104+UmeSalmaKhan@users.noreply.github.com>
2024-02-08 13:04:57 +0000

    upload

---
+++ 03_Control_Flow/switch.js
@@ -0,0 +1,56 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// Numerical switch try//
+
+// const month = "2"
+
+// switch (month) {
+//     case "1":
+//         console.log("January");
+//         break;
+//     case "2":
+//         console.log("feb");
+//         break;
+//     case "3":
+//         console.log("march");
+//         break;
+//     case "4":
+//         console.log("april");
+//         break;
+
+    // default:
+    //     console.log("default case match");
+    //     break;
+}
+
+
+// Alpetic try //
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
