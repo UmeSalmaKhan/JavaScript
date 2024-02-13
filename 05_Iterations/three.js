2e554ee4dcd8455864bd4025aea1bab26d0b62fa
UmeSalmaKhan <68366104+UmeSalmaKhan@users.noreply.github.com>
2024-02-13 11:50:30 +0000

    maps

--- 05_Iterations/three.js
+++ 05_Iterations/three.js
@@ -20,13 +20,17 @@ const map = new Map()
 map.set('IN', "India")
 map.set('USA', "United States of America")
 map.set('Fr', "France")
-map.set('IN', "India")
+map.set('IN', "India")   //Maps are known for giving unique values not duplicate I"India" is give twice but it prints only once
 
 
 // console.log(map);
 
+for (const key of map) {
+    console.log(key);    // Give the output in form of array ex: [ ]
+}
+
 for (const [key, value] of map) {
-    // console.log(key, ':-', value);
+    // console.log(key, ':-', value);   // Give the output sepeately Key:- Object "ex: IN:- India"
 }
 
 const myObject = {
